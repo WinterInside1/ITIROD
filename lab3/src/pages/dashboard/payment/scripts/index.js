7c2dee3522a24a84879a58b7e80cf197b5104e72
WinterInside1 <dbw2250@gmail.com>
2022-05-18 05:13:51 +0300

    Delete lab3 directory

--- lab3/src/pages/dashboard/payment/scripts/index.js
+++
@@ -1,6 +0,0 @@
-const cardForm = document.getElementById('card-form');
-
-cardForm.addEventListener('submit', (e) => {
-  e.preventDefault();
-  window.location.href = `/dashboard/calendar?paymentStatus=success`;
-});
